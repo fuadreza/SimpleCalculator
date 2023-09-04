11a3a925c44965d38d836d273d23233a454c027f
fuadreza <fuadreza1011@gmail.com>
2023-09-04 16:12:09 +0700

    Initial Commit

---
+++ SimpleCalculator/SimpleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalculatorApp.swift
+//  SimpleCalculator
+//
+//  Created by Fuad Reza Pahlevi on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
