7ef2535ce509aa2d22e623f2a7731d7641f253a4
fuadreza <fuadreza1011@gmail.com>
2023-09-04 18:48:33 +0700

    initial commit

--- SimpleCalculator/ContentView.swift
+++ SimpleCalculator/ContentView.swift
@@ -6,16 +6,78 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State var inputA: String = ""
+    @State var inputB: String = ""
+    @State var result: String = "-"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Number A")
+            TextField("Placeholder", text: $inputA)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(UIKeyboardType.numberPad)
+                .onReceive(Just(inputA)) { newValue in
+                    let filtered = newValue.filter {
+                        "0123456789".contains($0)
+                    }
+                    if filtered != newValue {
+                        self.inputA = filtered
+                    }
+                }
+            Text("Number B")
+            TextField("Placeholder", text: $inputB)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+            HStack(alignment: .center) {
+                Button("+") {
+                    calculate(operatorSymbol: "+")
+                }
+                .padding()
+                .buttonStyle(.bordered)
+                Button("-") {
+                    calculate(operatorSymbol: "-")
+                }
+                .padding()
+                .buttonStyle(.bordered)
+                Button("*") {
+                    calculate(operatorSymbol: "*")
+                }
+                .padding()
+                .buttonStyle(.bordered)
+                Button("/") {
+                    calculate(operatorSymbol: "/")
+                }
+                .padding()
+                .buttonStyle(.bordered)
+            }
+            .frame(maxWidth: .infinity)
+            Text("Result").bold().frame(maxWidth: .infinity)
+            Text(result).frame(maxWidth: .infinity)
         }
         .padding()
+    }
+    
+    func calculate(operatorSymbol: String) {
+        if operatorSymbol == "+" {
+            if (!inputA.isEmpty && !inputB.isEmpty) {
+                result = String((Int(inputA) ?? 0) + (Int(inputB) ?? 0))
+            }
+        } else if operatorSymbol == "-" {
+            if (!inputA.isEmpty && !inputB.isEmpty) {
+                result = String((Int(inputA) ?? 0) - (Int(inputB) ?? 0))
+            }
+        }else if operatorSymbol == "*" {
+            if (!inputA.isEmpty && !inputB.isEmpty) {
+                result = String((Int(inputA) ?? 0) * (Int(inputB) ?? 0))
+            }
+        } else if operatorSymbol == "/" {
+            if (!inputA.isEmpty && !inputB.isEmpty) {
+                result = String(Double((Double(inputA) ?? 0) / (Double(inputB) ?? 0)))
+            }
+        }
     }
 }
 
